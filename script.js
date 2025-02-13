64093ca71fb9c0712554293d8d573c1132d9dd14
jerlin-shabi <jerlinshabi.cs22@stellamaryscoe.edu.in>
2025-02-13 11:10:41 +0530

    some bla bla bla

--- script.js
+++ script.js
@@ -48,17 +48,30 @@ document.addEventListener("DOMContentLoaded", function () {
     function generateTicket(name, email, github, avatar) {
         // Clear existing ticket content
         ticketContainer.innerHTML = `
+            <div class="heading">
+            <h1>Congrats, ${name}!</h1>
+            <h1>Your ticket is ready.</h1>
+            </div>
+            <div class="sub-heading">
+            <p>We've emailed your ticket to</p>
+            <p>${email} and will send updates in</p>
+            <p>the run up to the event.</p>
+            </div>
             <div class="ticket">
-                <div class="ticket-header">
-                    <img src="assests/images/logo-full.svg" alt="Conference Logo" class="ticket-logo">
-                </div>
-                <p style="text-align:left;">Jan 31, 2025 / Shabi, TX</p>
-                <div class="ticket-content">
-                    <img src="${avatar}" alt="Avatar" class="avatar">
-                    <p><strong></strong> ${name}</p>
-                    <p><strong></strong> ${email}</p>
-                    <p><strong></strong> ${github}</p>
-                </div>
+            <div class="ticket-header">
+                <img src="assets/images/logo-full.svg" alt="Conference Logo" class="ticket-logo">
+            </div>
+            <p class="shabi" style="text-align:left;">Jan 31, 2025 / Shabi, TX</p>
+            <div class="ticket-content">
+                <img src="${avatar}" alt="Avatar" class="avatar">
+            </div>
+            <div>
+                <p class="name-ticket">${name}</p>
+            </div>
+            <div class="github-info">
+                <img src="assests/images/icon-github.svg" class="git-img" alt="GitHub Icon">
+                <p class="github-username">${github}</p>
+            </div>
             </div>
         `;
 
@@ -66,4 +79,6 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".container").classList.add("hidden");
         ticketContainer.classList.remove("hidden");
     }
+
+    
 });
