dc3ee0dcd1f5db62ed790a88d6c9faaa9aab3296
jerlin-shabi <jerlinshabi.cs22@stellamaryscoe.edu.in>
2025-02-13 15:21:44 +0530

    some bla bla bla

--- script.js
+++ script.js
@@ -46,34 +46,37 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function generateTicket(name, email, github, avatar) {
-        // Clear existing ticket content
         ticketContainer.innerHTML = `
             <div class="heading">
-            <h1>Congrats, ${name}!</h1>
-            <h1>Your ticket is ready.</h1>
+                <h1>Congrats, ${name}!</h1>
+                <h1>Your ticket is ready.</h1>
             </div>
             <div class="sub-heading">
-            <p>We've emailed your ticket to</p>
-            <p>${email} and will send updates in</p>
-            <p>the run up to the event.</p>
+                <p>We've emailed your ticket to</p>
+                <p>${email} and will send updates in</p>
+                <p>the run-up to the event.</p>
             </div>
             <div class="ticket">
-            <div class="ticket-header">
-                <img src="assets/images/logo-full.svg" alt="Conference Logo" class="ticket-logo">
-            </div>
-            <p class="shabi" style="text-align:left;">Jan 31, 2025 / Shabi, TX</p>
-            <div class="ticket-content">
-                <img src="${avatar}" alt="Avatar" class="avatar">
-            </div>
-            <div>
+                 <img src="assests/images/logo-full.svg" alt="Conference Logo" class="ticket-logo">
+                 <p class="shabi">Jan 31, 2025 / Shabi, TX</p>
+                <div class="ticket-content">
+                    <img src="${avatar}" alt="Avatar" class="avatar">
+                <div class="github-info">
                 <p class="name-ticket">${name}</p>
-            </div>
-            <div class="github-info">
-                <img src="assests/images/icon-github.svg" class="git-img" alt="GitHub Icon">
-                <p class="github-username">${github}</p>
-            </div>
+                    <div class="info">
+                        <img src="assests/images/icon-github.svg" class="git-img" alt="GitHub Icon">
+                        <p class="github-username">${github}</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                
+                
+                </div>
+        </div>
             </div>
         `;
+    
+
 
         // Hide the form and show the ticket
         document.querySelector(".container").classList.add("hidden");
